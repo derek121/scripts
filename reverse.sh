da19f44694c927c85781bb8df34de83e3fcd10d3
Derek Brown <derekbrown121@gmail.com>
2012-04-22 14:54:45 -0400

    Adding file

---
+++ reverse.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function reverse() {
+  s="$*"
+
+  if [ -z "$s" ]; then
+    return 0
+  fi
+
+  x="${s:0:1}"
+  xs="${s:1:$(( ${#s} - 1 ))}"
+
+  echo -n "$(reverse "$xs")""$x"
+  return 0
+}
+
+echo "$(reverse "$*")"
+
